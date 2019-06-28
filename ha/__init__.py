5f6f8189e1a58c70681df9c046eb52d63b8c191b
Dielson Sales <dielson@ilhasoft.com.br>
2019-06-28 09:56:44 -0300

    Create Hausa language for use in spaCy

---
+++ ha/__init__.py
@@ -0,0 +1,36 @@
+# coding: utf8
+from __future__ import unicode_literals
+
+from .tokenizer_exceptions import TOKENIZER_EXCEPTIONS
+from .stop_words import STOP_WORDS
+from .lex_attrs import LEX_ATTRS
+from .lemmatizer import LOOKUP
+from .norm_exceptions import NORM_EXCEPTIONS
+
+from ..tokenizer_exceptions import BASE_EXCEPTIONS
+from .punctuation import TOKENIZER_INFIXES, TOKENIZER_PREFIXES
+from ..norm_exceptions import BASE_NORMS
+from ...language import Language
+from ...attrs import LANG, NORM
+from ...util import update_exc, add_lookups
+
+
+class HausaDefaults(Language.Defaults):
+    lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
+    lex_attr_getters[LANG] = lambda text: 'ha'
+    lex_attr_getters[NORM] = add_lookups(Language.Defaults.lex_attr_getters[
+                                         NORM], BASE_NORMS, NORM_EXCEPTIONS)
+    lex_attr_getters.update(LEX_ATTRS)
+
+    tokenizer_exceptions = update_exc(BASE_EXCEPTIONS, TOKENIZER_EXCEPTIONS)
+    stop_words = STOP_WORDS
+    lemma_lookup = LOOKUP
+    infixes = TOKENIZER_INFIXES
+    prefixes = TOKENIZER_PREFIXES
+
+
+class Hausa(Language):
+    lang = 'ha'
+    Defaults = HausaDefaults
+
+__all__ = ['Hausa']
---
+++ ha/tokenizer_exceptions.py
@@ -0,0 +1,6 @@
+# encoding: utf8
+from __future__ import unicode_literals
+from ...symbols import ORTH, NORM
+
+
+TOKENIZER_EXCEPTIONS = dict()
