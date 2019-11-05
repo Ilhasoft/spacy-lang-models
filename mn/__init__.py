e41de73c025149b93eaf4bd0fd1104b50d5fdb30
Dielson Sales <dielson@ilhasoft.com.br>
2019-11-05 16:47:53 -0300

    Standardize language definitions and remove unecessary imports

--- mn/__init__.py
+++ mn/__init__.py
@@ -3,10 +3,8 @@ from __future__ import unicode_literals
 
 from .tokenizer_exceptions import TOKENIZER_EXCEPTIONS
 from .stop_words import STOP_WORDS
-from .norm_exceptions import NORM_EXCEPTIONS
-from .morph_rules import MORPH_RULES
 from .lemmatizer import LOOKUP
-
+from .norm_exceptions import NORM_EXCEPTIONS
 from ..tokenizer_exceptions import BASE_EXCEPTIONS
 from ..norm_exceptions import BASE_NORMS
 from ...language import Language
@@ -17,7 +15,7 @@ from ...util import update_exc, add_lookups
 class MongolianDefaults(Language.Defaults):
     lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
     lex_attr_getters[LANG] = lambda text: 'mn'
-    lex_attr_getters[NORM] = add_lookups(Language.Defaults.lex_attr_getters[NORM], BASE_NORMS)
+    lex_attr_getters[NORM] = add_lookups(Language.Defaults.lex_attr_getters[NORM], BASE_NORMS, NORM_EXCEPTIONS)
 
     tokenizer_exceptions = update_exc(BASE_EXCEPTIONS, TOKENIZER_EXCEPTIONS)
     stop_words = set(STOP_WORDS)
